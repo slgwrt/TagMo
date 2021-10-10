a1dec0c67670b94254b6a5dea3ec3890f1e9c89f
slgwrt <86700478+slgwrt@users.noreply.github.com>
2021-10-10 20:36:22 +0800

    Update TagWriter.java

--- app/src/main/java/com/hiddenramblings/tagmo/TagWriter.java
+++ app/src/main/java/com/hiddenramblings/tagmo/TagWriter.java
@@ -213,10 +213,20 @@ public class TagWriter {
         TagMo.Debug(TAG, Util.bytesToHex(tagData));
         return tagData;
     }
+    
+    public static String byteToHex(byte[] bytes){
+        String strHex = "";
+        StringBuilder sb = new StringBuilder("");
+        for (int n = 0; n < bytes.length; n++) {
+            strHex = Integer.toHexString(bytes[n] & 0xFF);
+            sb.append((strHex.length() == 1) ? "0" + strHex : strHex); // 每个字节由两个字符表示，位数不够，高位补0
+        }
+        return sb.toString().trim();
+    }
 
     static void writePages(NTAG215 tag, int pagestart, int pageend, byte[][] data) throws Exception {
         byte[] bigdata = new byte[(pageend-pagestart+1)*6];
-        for (int i = pagestart; i <= pageend; i++) {
+        for (int i = pagestart,j = 0; i <= pageend; i++,j ++) {
             tag.writePage(i, data[i]);
             TagMo.Debug(TAG, R.string.write_page, String.valueOf(i));
            //writepage-cdoe
